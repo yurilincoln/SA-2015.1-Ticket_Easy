d2d3cb7a9c6103c3e31d4aaccaa5e3ba49b924ca
Yuri Lincoln <yurilxmp@gmail.com>
2015-06-01 17:33:29 -0300

    Teste de um novo menu.

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,5 +1,14 @@
 ( function( $ ) {
 $( document ).ready(function() {
 $('#cssmenu').prepend('<div id="bg-one"></div><div id="bg-two"></div><div id="bg-three"></div><div id="bg-four"></div>');
-});
+var nav = $('.nav-container');
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 136) {
+        nav.addClass("f-nav");
+    } else {
+        nav.removeClass("f-nav");
+    }
+})
+});;
 } )( jQuery );
