79c4843bea5ab705fb945c28609af7e91cb247eb
yurilincoln <yurilxmp@gmail.com>
2015-06-10 21:06:06 -0300

    Configuração de menu.

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,26 +1,32 @@
-( function( $ ) {
-$( document ).ready(function() {
-$('#cssmenu').prepend('<div id="bg-one"></div><div id="bg-two"></div><div id="bg-three"></div><div id="bg-four"></div>'); });
+(function($) {
+	$(document)
+			.ready(
+					function() {
+						$('#cssmenu')
+								.prepend(
+										'<div id="bg-one"></div><div id="bg-two"></div><div id="bg-three"></div><div id="bg-four"></div>');
+					});
 
-$(document).ready(function() {
-	// mostra esconde menu
-	  $("a.mostra-esconde-menu").click(function(){
-	      $(".menu_mluv_retratil").toggleClass('esconder');
-	      if ($(this).text() == "esconder ▲")
-	       $(this).text("menu ▼")
-	        else
-	       $(this).text("esconder ▼");
-	  });
+	$(document).ready(function() {
+		// mostra esconde menu
+		$("a.mostra-esconde-menu").click(function() {
+			$(".menu_mluv_retratil").toggleClass('esconder');
+			if ($(this).text() == "esconder ▲")
+				$(this).text("menu ▼")
+			else
+				$(this).text("esconder ▼");
+		});
 	});
 	// menu ao rolar
-	$(window).bind('scroll', function () {
-	      if ($(window).scrollTop() > 350) {
-	        $(".menu_mluv_retratil").addClass('esconder');
-	        $("a.mostra-esconde-menu").text('menu ▼');
-	      } else if ($(window).scrollTop() < 350) {
-	        $(".menu_mluv_retratil").removeClass('esconder');
-	        $("a.mostra-esconde-menu").text('esconder ▲');
-	      }
+	$(window).bind('scroll', function() {
+		if ($(window).scrollTop() > 350) {
+			$(".menu_mluv_retratil").addClass('esconder');
+			$("a.mostra-esconde-menu").text('menu ▼');
+		} else if ($(window).scrollTop() < 350) {
+			$(".menu_mluv_retratil").removeClass('esconder');
+			$("a.mostra-esconde-menu").text('esconder ▲');
+		}
 	});
 
-} )( jQuery );
+
+})(jQuery);
